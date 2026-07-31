d541f22022f8c78efc5085ffaa814c8ea7ba91d7
Christian Sueiras <1381470+csueiras@users.noreply.github.com>
2026-07-31 15:43:26 -0400

    feat: add keyword, contextual keyword, and annotation scopes

---
+++ tests/grammar/annotations.fs
@@ -0,0 +1,10 @@
+# SYNTAX TEST "source.foundryscript"
+
+@export
+# <- entity.name.function.decorator.foundryscript
+
+@onready var node = null
+# <- entity.name.function.decorator.foundryscript
+
+@my_custom(1, 2)
+# <- entity.name.function.decorator.foundryscript
---
+++ tests/grammar/contextual-keywords.fs
@@ -0,0 +1,60 @@
+# SYNTAX TEST "source.foundryscript"
+
+extend int uses Describable:
+# <- keyword.declaration.extend.foundryscript
+#      ^^^ entity.name.type.foundryscript
+
+async func fetch() -> Coroutine[int]:
+# <- storage.modifier.async.foundryscript
+
+annotation my_marker targets CLASS, METHOD
+# <- keyword.declaration.annotation.foundryscript
+#                    ^^^^^^^ keyword.other.targets.foundryscript
+#                            ^^^^^^^^^^^^^ support.constant.target.foundryscript
+
+var extend = 1
+#   ^^^^^^ - keyword
+
+var async = 2
+#   ^^^^^ - storage.modifier
+
+if true:
+    extend int uses Describable:
+    #   ^^^^^^ - keyword
+
+if true:
+    annotation my_marker targets CLASS, METHOD
+    #   ^^^^^^^^^^ - keyword
+
+func handle(async: int):
+#           ^^^^^ - storage.modifier
+
+foo(async, 1)
+#   ^^^^^ - storage.modifier
+
+var config = {async = 1}
+#              ^^^^^ - storage.modifier
+
+var handler = get
+#             ^^^ - storage.modifier.accessor.foundryscript
+
+var targets = 5
+#   ^^^^^^^ - keyword.other.targets.foundryscript
+
+var class_name_helper = 1
+#   ^^^^^^^^^^^^^^^^^^ - keyword
+
+var iffy = true
+#   ^^^^ - keyword
+
+var information = 1
+#   ^^^^^^^^^^^ - keyword
+
+var nothing = 1
+#   ^^^^^^^ - keyword
+
+func async_handler():
+#    ^^^^^^^^^^^^^ - storage.modifier
+
+extended = 1
+# <- - keyword
